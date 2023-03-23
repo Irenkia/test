1d53c0b70d236bc01c200e9c1927c760c7c7a630
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-03-23 13:52:10 +0100

    create react app

---
+++ my-app/src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./app.css";
+
+const App = ()=>{
+    return(
+        <div className='main_div'>
+
+        </div>
+    )
+
+}
+export default App;
---
+++ my-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from "./components/app/app";
+
+// ReactDOM.render(<App/>,document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
+console.log("Hello");
